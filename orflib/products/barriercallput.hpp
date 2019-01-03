aef05da7d19a92808b55e94a94a96613e1b4954c
jrfan94 <jrfan94@users.noreply.github.com>
2019-01-03 11:23:52 -0500

    Test: Freq adjustment
    Buggy version of frequency-adjusted pricing. To fix the truncation of days in front

--- orflib/products/barriercallput.hpp
+++ orflib/products/barriercallput.hpp
@@ -71,43 +71,31 @@ BarrierCallPut::BarrierCallPut(int payoffType, double strike, double barrier, st
     time_factor = 52.0;
     break;
   case BarrierCallPut::Freq::DAILY:
-    time_factor = 365.0;
+    time_factor = DAYS_PER_YEAR;
     break;
   default:
     ORF_ASSERT(0, "BarrierCallPut: unknown barrier option frequency type!");
   }
 
   // number of fixing times determined by Freq = time_factor
-  //nfixings = timeToExp * time_factor + 1;
-  //double rounded_up_nfixings = ceil(nfixings);
-  //ORF_ASSERT(rounded_up_nfixings > 0, "BarrierCallPut: the option has expired!");
+  nfixings = timeToExp * time_factor + 1;
+  size_t rounded_up_nfixings = ceil(nfixings);
+  ORF_ASSERT(rounded_up_nfixings > 0, "BarrierCallPut: the option has expired!");
 
-  //double stub_time = rounded_up_nfixings > nfixings ? (timeToExp - floor(timeToExp)) / time_factor : 0.0;
-  //double offset = rounded_up_nfixings > nfixings ? 1.0 : 0.0;
+  double stub_time = rounded_up_nfixings > nfixings ? (timeToExp - floor(timeToExp)) / time_factor : 0.0;
+  double offset = rounded_up_nfixings > nfixings ? 1.0 : 0.0;
 
   // set the fixing times
-  //fixTimes_.resize(rounded_up_nfixings);
-  //fixTimes_[0] = 0.0;
+  fixTimes_.resize(rounded_up_nfixings);
+  fixTimes_[0] = 0.0;
 
-  //for (size_t i = 0; i < rounded_up_nfixings - 1; ++i)
-  //  fixTimes_[i + offset] = i / time_factor + stub_time;
-  //fixTimes_[nfixings - 1] = timeToExp_;
-
-  // payTimes_.resize(1);
-  // payTimes_[0] = timeToExp_;
-
-  // this product generates only one payment
-  // payAmounts_.resize(1);
-  nfixings = static_cast<size_t>(timeToExp * DAYS_PER_YEAR) + 1;
-  ORF_ASSERT(nfixings > 0, "BarrierCallPut: the option has expired!");
-  fixTimes_.resize(nfixings);
-  for (size_t i = 0; i < nfixings - 1; ++i)
-	  fixTimes_[i] = i / DAYS_PER_YEAR;
+  for (size_t i = 0; i < rounded_up_nfixings - 1; ++i)
+    fixTimes_[i + offset] = i / time_factor + stub_time;
   fixTimes_[nfixings - 1] = timeToExp_;
 
   payTimes_ = fixTimes_;
 
-  // this product could generate a payment on each day between now and expiration.
+  // this product takes either its continuation value or zero at the fixing times
   payAmounts_.resize(payTimes_.size());
 }
 
@@ -120,7 +108,6 @@ inline void BarrierCallPut::eval(Matrix const& pricePath)
 
 inline void BarrierCallPut::eval(size_t idx, Vector const& spots, double contValue)
 {
-  // the continuation value is not used
 	double spot = spots[0];
 
 	if (idx == payAmounts_.size() - 1) { // this is the last index
@@ -139,99 +126,7 @@ inline void BarrierCallPut::eval(size_t idx, Vector const& spots, double contVal
 		else {
 			payAmounts_[idx] = spot > barrier_ ? contValue : 0;
 		}
-		
-		
-		//double intrinsicValue = (spot - strike_) * payoffType_;
-		//intrinsicValue = intrinsicValue >= 0.0 ? intrinsicValue : 0.0;
-		//payAmounts_[idx] = contValue >= intrinsicValue ? contValue : 0;
-		// zero out the amounts after this index
-		//for (size_t j = idx + 1; j < payAmounts_.size(); ++j)
-		//	payAmounts_[j] = 0.0;
 	}
-
-  // if (idx == fixTimes_.size() - 1) { // this is the last index
-  //  double payoff = (S_T - strike_) * payoffType_;
-  //  payAmounts_[0] = payoff > 0.0 && contValue != 1.0 ? payoff : 0.0;
-  //}
-  //else {  // this is not the last index, check the exercise condition
-  //  contValue = barrier_type_[0] == 'u' && S_T >= barrier_ && contValue != 1.0 ? 1.0 : 0.0;
-  //  contValue = barrier_type_[0] == 'd' && S_T <= barrier_ && contValue != 1.0 ? 1.0 : 0.0;
-  //}
-	//if (idx == payTimes_.size() - 1) { // this is the last index
-	//	if (payoffType_ == 1) {
-	//		if (barrier_type_[0] == 'u') {
-	//			if (barrier_type_[1] == 'o') {
-	//				payAmounts_[idx] = spot >= strike_ && spot < barrier_ ? spot - strike_ : 0.0;
-	//			}
-	//			else {
-	//				ORF_ASSERT(0, "Not implemented!");
-	//			}
-	//		}
-	//		else {
-	//			if (barrier_type_[1] == 'o') {
-	//				payAmounts_[idx] = spot >= strike_ && spot > barrier_ ? spot - strike_ : 0.0;
-	//			}
-	//			else {
-	//				ORF_ASSERT(0, "Not implemented!");
-	//			}
-	//		}
-	//	}
-	//	else {
-	//		if (barrier_type_[0] == 'u') {
-	//			if (barrier_type_[1] == 'o') {
-	//				payAmounts_[idx] = spot <= strike_ && spot < barrier_ ? strike_ - spot : 0.0;
-	//			}
-	//			else {
-	//				ORF_ASSERT(0, "Not implemented!");
-	//			}
-	//		}
-	//		else {
-	//			if (barrier_type_[1] == 'o') {
-	//				payAmounts_[idx] = spot <= strike_ && spot > barrier_ ? strike_ - spot : 0.0;
-	//			}
-	//			else {
-	//				ORF_ASSERT(0, "Not implemented!");
-	//			}
-	//		}
-	//	}
-	//}
-	//else {  // this is not the last index, check whether barrier has been hit
-	//	//double intrinsicValue = (spot - strike_) * payoffType_;
-	//	//intrinsicValue = intrinsicValue >= 0.0 ? intrinsicValue : 0.0;
-	//	//payAmounts_[idx] = contValue >= intrinsicValue ? contValue : intrinsicValue;
-	//	/////// zero out the amounts after this index
-	//	//for (size_t j = idx + 1; j < payAmounts_.size(); ++j)
-	//	//	payAmounts_[j] = 0.0;
-
-	//	if (payoffType_ == 1) {
-	//		if (barrier_type_[0] == 'u') {
-	//			if (barrier_type_[1] == 'o' && spot >= barrier_) {
-	//				for (size_t j = idx + 1; j < payAmounts_.size(); ++j)
-	//					payAmounts_[j] = 0.0;
-	//			}
-	//		}
-	//		else {
-	//			if (barrier_type_[1] == 'o' && spot <= barrier_) {
-	//				for (size_t j = idx + 1; j < payAmounts_.size(); ++j)
-	//					payAmounts_[j] = 0.0;
-	//			}
-	//		}
-	//	}
-	//	else {
-	//		if (barrier_type_[0] == 'u') {
-	//			if (barrier_type_[1] == 'o' && spot >= barrier_) {
-	//				for (size_t j = idx + 1; j < payAmounts_.size(); ++j)
-	//					payAmounts_[j] = 0.0;
-	//			}
-	//		}
-	//		else {
-	//			if (barrier_type_[1] == 'o' && spot <= barrier_) {
-	//				for (size_t j = idx + 1; j < payAmounts_.size(); ++j)
-	//					payAmounts_[j] = 0.0;
-	//			}
-	//		}
-	//	}
-	//}
 }
 
 END_NAMESPACE(orf)
